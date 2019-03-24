5ce0008e834729816b73cbbc83a262755285ce91
KingDavid2 <nausito@gmail.com>
2019-03-24 11:48:35 -0600

    CheckoutItem remove

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,32 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import store from '../store';
+import CheckoutItem from './CheckoutItem'
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+  mainContainer: {
+    paddingTop: 24,
+  }
+});
 
 class Checkout extends Component {
   constructor(){
@@ -9,21 +36,60 @@ class Checkout extends Component {
       cart: []
     }
 
-    store.subscribe(() => {
+    this.removeFromCart = this.removeFromCart.bind(this)
+  }
+
+  componentDidMount(){
+    // this.unsubscribe = store.subscribe(() => {
       this.setState({
         cart: store.getState().cart
-      })
+      });
+    // });
+    // debugger
+  }
+  componentDidUpdate(){
+    // debugger
+  }
+
+  componentWillUnmount(){
+    // this.unsubscribe();
+  }
+
+  removeFromCart(product){
+    store.dispatch({
+        type: "REMOVE_FROM_CART",
+        product: product
+    });
+
+    this.setState({
+      cart: store.getState().cart
     });
   }
 
   render() {
     return (
-      <div className="">
-
+      <div className="" style={{ padding: 54 }}>
+        <Grid container
+        justify='center'
+        >
+          <Grid container
+            className=''
+            direction="column"
+            xs={8}
+            >
+            {this.state.cart
+              .map(item =>
+                (<CheckoutItem
+                  item = {item}
+                  removeFromCart = {this.removeFromCart}
+                  />)
+              )}
+          </Grid>
+        </Grid>
 
       </div>
     );
   }
 }
 
-export default Checkout;
+export default withStyles(styles)(Checkout);
---
+++ src/components/CheckoutItem.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import MaterialIcon, {colorPalette} from 'material-icons-react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Fab from '@material-ui/core/Fab';
+import store from '../store';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    margin: 'auto',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+});
+
+class CheckoutItem extends Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    removeFromCart(product){
+        this.props.removeFromCart(product)
+    }
+
+    render() {
+        const { item } = this.props;
+        return (
+            <div className=''>
+            <Grid item>
+                
+            <Paper className=''
+            style={{ padding: 16 }}>
+                <Grid container spacing={16}>
+                <Grid item xs={4}>
+                    <ButtonBase className=''>
+                    <img className='' alt="complex" src={item.url} style={{ maxWidth: '100%' }}/>
+                    </ButtonBase>
+                </Grid>
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={16}>
+                    <Grid item xs>
+                        <Typography gutterBottom variant="title" fontSize={16}>
+                        <b>{item.title}</b>
+                        </Typography>
+                        <Typography gutterBottom variant="subheading">{item.artist.name}</Typography>
+                        <Typography color="textSecondary">Size: {item.weight} x {item.height}</Typography>
+                        <Typography color="textSecondary">ID: {item.id}</Typography>
+
+                    </Grid>
+                    <Grid item xs>
+                        <Typography style={{ cursor: 'pointer' }}>
+                          </Typography>
+                    </Grid>
+                    </Grid>
+                    <Grid item xs style={{textAlign: 'right'}}>
+                            <Typography variant="subheading">$ {item.price}
+                            </Typography>
+                        </Grid>
+                    <Grid container
+                    direction="column">
+                            <Grid item xs style={{textAlign: 'right'}}>
+                            <Fab color="secondary"
+                                    aria-label="Delete" 
+                                    className=''
+                                    onClick={() => this.removeFromCart(item)}>
+                                    
+                                <DeleteIcon />
+                            </Fab>
+                        </Grid>
+
+
+                    </Grid>
+
+                </Grid>
+                </Grid>
+            </Paper>
+            </Grid>  
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(CheckoutItem);
